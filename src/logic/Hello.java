b93432c013e485dde038767c04c9eca302ad7d1e
Pakapon Kaewmark <sleep191@gmail.com>
2015-08-20 16:27:43 +0700

    Update Template Code - Add method bubbleSort() (return the given array in an ascending order) - Add method arrayToString (return the content of array in single-line string)

--- src/logic/Hello.java
+++ src/logic/Hello.java
@@ -3,11 +3,33 @@ package logic;
 public class Hello {
 
 	public static void main(String[] args) {
+		
 		System.out.println(Hello.greetingMessage());
+		
+		int[] array = {5,4,3,2,1};
+		
+		System.out.println(Hello.arrayToString("Array", array));
+		array = Hello.bubbleSort(array);
+		System.out.println(Hello.arrayToString("Sorted Array", array));
 	}
 
 	public static String greetingMessage(){
-		// TODO Implement this method to return string "Hello Github".
+		// TODO Implement this method to return string "Hello GitHub".
 		return "";
+	}
+	
+	public static int[] bubbleSort(int[] array){
+		// TODO Implement this method to sort the given array in an ascending order and return the sorted array.
+		return array;
+	}
+	
+	public static String arrayToString(String title,int[] array){
+		
+		String str = title + " : ";
+		for (int i = 0;i < array.length;i++)
+		{
+			str += array[i] + " ";
+		}
+		return str;
 	}
 }
--- src/test/HelloTest.java
+++ src/test/HelloTest.java
@@ -9,7 +9,14 @@ public class HelloTest {
 
 	@Test
 	public void testGreetingMessage() {
-		assertEquals("Hello Github", Hello.greetingMessage());
+		assertEquals("Hello GitHub", Hello.greetingMessage());
+	}
+	
+	@Test
+	public void testBubbleSort(){
+		
+		assertArrayEquals(new int[]{1,2,3,4,5}, Hello.bubbleSort(new int[]{5,4,3,2,1}));
+		assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9,10}, Hello.bubbleSort(new int[]{10,5,9,4,7,2,6,1,8,3}));
 	}
 
 }
